27edd40a2554584c2a943d7722b88733fa2eaef0
Timothy Baraka <timothybaraka44@gmail.com>
2022-06-23 17:10:13 +0300

    My very first upload commit

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,21 @@
-const button5 = document.querySelector("#plusFive")
+window.onload = () =>{
+
+// const button5 = document.querySelector("#plusFive")
 const indicator = document.querySelector("#number-indicator")
 const disp = document.querySelector("#display-heading")
+const buttons = [...document.querySelectorAll("button")]
+// const buttons = Array.prototype.slice.call(buttonsArr)
 
-button5.addEventListener('click', () => {
-  disp.textContent = "added 5"
-  indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
+// button5.addEventListener('click', () => {
+//   disp.textContent = "added 5"
+//   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
+// })
+
+buttons.forEach(element =>{
+  element.addEventListener('click',()=>{
+    disp.textContent = `added ${(buttons.indexOf(element) + 1) * 5}`;
+    indicator.textContent = (parseInt(indicator.textContent) + ((buttons.indexOf(element) + 1) * 5)).toString();
 })
+});
+//console.log(buttons)
+}
